011002d847ae57703d30c881a7c68335494bda9c
justmalysa <justynamalysa98@gmail.com>
2022-01-02 15:34:43 +0100

    Add doxygen docs for motorcycle alarm library

--- src/motorcycle_alarm.h
+++ src/motorcycle_alarm.h
@@ -1,7 +1,18 @@
 #ifndef MOTORCYCLE_ALARM_H
 #define MOTORCYCLE_ALARM_H
 
+/**
+ * @brief Function for initializing motorcycle alarm.
+ *
+ * @param user_number Pointer to user phone number.
+*/
 void motorcycle_alarm_init(const char *user_number);
+
+/**
+ * @brief Function for measuring acceleration using BMA220 sensor.
+ *
+ * @note  Used only for selecting the appropriate sensitivity value of the device.
+ */
 void bma220_measure(void);
 
 #endif /* MOTORCYCLE_ALARM_H */
